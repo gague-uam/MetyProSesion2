66354309a2d55c6457327b460b9805601d4a0a91
gague-UAM <ggpaiz@uamv.edu.ni>
2024-08-26 08:52:47 -0600

    Sesion 3, primer ejercicio en escritorio

---
+++ MyApp1/MyApp1/FrmPrincipal.cs
@@ -0,0 +1,29 @@
+﻿using MyApp1.clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEvaluar_Click(object sender, EventArgs e)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = tbNombre1.Text;
+            persona.FechaNac = dtpFechaNac.Value;
+            MessageBox.Show(persona.EvaluarEdad());
+        }
+    }
+}
